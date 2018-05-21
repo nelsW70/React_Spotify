1b6466ce6dce7b73315132bb142dc027043c4b29
nelsW70 <nelsfoltz@gmail.com>
2018-05-21 19:32:02 -0400

    added Gallery component and styled it

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 import Profile from './Profile';
+import Gallery from './Gallery';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       query: '',
-      artist: null
+      artist: null,
+      tracks: []
     }
   }
 
@@ -17,7 +19,7 @@ class App extends Component {
   const BASE_URL = 'https://api.spotify.com/v1/search?';
   let FETCH_URL = BASE_URL + 'q=' + this.state.query + '&type=artist&limit=1';
   const ALBUM_URL = 'https://api.spotify.com/v1/artists/';
-  const accessToken = 'BQDFWl2oLsJmpLmc5o0O_xOL_7DkHAoctsK_QYISEQKPur8nE6o-7zNIcxmHtD23dKqNgznirG81avGjOLfGqikKzxgq53hnKUrvho0GRqiMK27QPVE8CT2-t9GYFzHp3yDATVClGRl_aARobxTrHFeO2uhf&refresh_token=AQA1cyq1e1_V9xsN4fphulgoRfAp5GglKRdvSH5lh4He14y10ltz2gdgO2nAuifixAs99Q-y5fK0X65NuSAGP0KzsfgPCBLHcxze4maOcnNRRUZFMnVjoMITBSPHTxJx-0I'
+  const accessToken = 'BQDPVEsQ1TIrO-i77Ogk6xL221K1IUoh0i1ZQ3cwJP-wwc96NakFW4YuUtn-_a9GJQc9-jPQZAz2Y3R_EyHXJ_pL57AgJSJ_vJIrq8L9r8Hjw_XZ_nEEfElndKU6GT5Z42DEjOtoW4TXwjAGJKg-F39H3x-w&refresh_token=AQAUlhCVmtVcE8PW640REqYeiVohmabm63RUgnQH2Cmux4W4TCSRwtGRwXrYKEkz5UKarwM-KaXTM5t7dnA47Z7FNk-0UbxxxNOoVIu-uuvwPMJCdR24xn4MXGSBQfa0cY0'
   // const myHeaders = new Headers();
   const myOptions = {
     method: 'GET',
@@ -28,7 +30,7 @@ class App extends Component {
     cache: 'default'
   };
 
-  fetch(FETCH_URL, myOptions )
+  fetch(FETCH_URL, myOptions)
     .then(response => response.json())
     .then(json => {
       const artist = json.artists.items[0];
@@ -40,6 +42,8 @@ class App extends Component {
       .then(response => response.json())
       .then(json => {
         console.log('artist\'s top tracks:', json);
+        const { tracks } = json;
+        this.setState({tracks});
       })
     });
 }
@@ -73,11 +77,10 @@ class App extends Component {
                 <Profile
                   artist={this.state.artist}
                   />
-                  <div className="Gallery">
-                    Gallery
-                  </div>
+                  <Gallery
+                    tracks={this.state.tracks}
+                  />
               </div>
-
             : <div></div>
           }
       </div>
